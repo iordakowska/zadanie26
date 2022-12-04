7aea7eaf0c30770a67d952014cb8809922e6847f
i.ordakowska <i.ordakowska@gmail.com>
2022-12-04 19:42:36 +0100

    stworzenie projektu

---
+++ src/main/java/com/example/zadanie26/Zadanie26Application.java
@@ -0,0 +1,13 @@
+package com.example.zadanie26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie26Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie26Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zadanie26/Zadanie26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zadanie26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie26ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
